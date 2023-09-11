944e43a69d642011c77d1b3997c846969fdf065d
Ernesto Alcaraz Andrade <ernesto.alcaraz@rappi.com>
2023-09-11 09:56:11 -0600

    = Remove methods nolonger support by ruby.

--- lib/graphql/language/nodes/deep_freeze_ext.rb
+++ lib/graphql/language/nodes/deep_freeze_ext.rb
@@ -10,11 +10,11 @@ module GraphQL
         #
         # Returns self Node.
         def deep_freeze
-          self.class.child_attributes.each do |attr_name|
+          self.class.class_attribute.each do |attr_name|
             public_send(attr_name).freeze.each(&:deep_freeze)
           end
 
-          self.class.scalar_attributes.each do |attr_name|
+          self.class.class_attribute.each do |attr_name|
             object = public_send(attr_name)
             object.freeze if object
           end
